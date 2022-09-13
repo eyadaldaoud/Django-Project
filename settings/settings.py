e475b746732033638e56fbf8c2f4f70a925922da
UGoingNoWhereBoy <ugnw20@gmail.com>
2022-09-13 05:02:11 -0700

    Deleted extra files

--- settings/settings.py
+++ settings/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-13+y)xp6c8qz=&!!t_tb3g$(@sr*t7m+ra6j&8597)j9cw=2on'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
